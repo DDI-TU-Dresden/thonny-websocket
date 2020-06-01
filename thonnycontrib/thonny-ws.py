f308261e4156c04ae8edf9bb5c232a75567a6c8c
niklaskeerl <niklas.keerl@mailbox.org>
2020-06-01 19:50:21 +0200

    Add support to communicate with multiple servers
    This closes #1 .

--- thonnycontrib/thonny-ws.py
+++ thonnycontrib/thonny-ws.py
@@ -1,93 +1,224 @@
 from thonny import get_workbench
 import websocket
-import threading
+from threading import Thread
 from tkinter.simpledialog import askstring
 
 
 def on_message(ws, message):
     """This method is called if the client received a message.
+    
+    :param ws: WebSocketApp object
 
-        Returns:
-            None
-        """
-    print("Message received: " + message)
+    :param message: message object represented as string
+
+    Returns:
+        None
+    """
+    print("Message from " + ws.url + " received: " + message)
 
 
 def on_error(ws, error):
     """This method is called if the client received an error.
+    
+    :param ws: WebSocketApp object
 
-        Returns:
-            None
-        """
-    print(error)
+    :param error: error object
+
+    Returns:
+        None
+    """
+    print("Error from " + ws.url + " received: " + error)
 
 
 def on_close(ws):
     """This method is called after the connection is closed.
+    
+    :param ws: WebSocketApp object
 
-        Returns:
-            None
-        """
-    print("Connection closed")
+    Returns:
+        None
+    """
+    print("Connection closed to server: " + ws.url)
 
 
 def on_open(ws):
     """This method is called after the connection is opened.
+    
+    :param ws: WebSocketApp object
 
-        Returns:
-            None
-        """
-    print("Connection opened")
+    Returns:
+        None
+    """
+    print("Connection opened to server: " + ws.url)
 
 
 def run_in_background():
-    """This method has to be called in a new thread because it blocks the current thread.
+    """This method has to be called in a new thread because it blocks the current
+    thread. It will listen for messages from the latest added WebSocketApp object.
 
-        Returns:
-            None
-        """
+    Returns:
+        None
+    """
+    wsList = Singleton.getInstance().get_wsList()
+    ws = wsList[len(wsList) - 1]
     ws.run_forever()
 
 
-def send_hello():
-    """This method gets called if the ws_send item is called in the tools menu.
+class Singleton:
+    """This Singleton class is needed because the list of WebSocketApp objects is shared.
+
+    __instance: The current instance of this class.
+        """
+
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """This is an unmodified Singleton.getInstance() method. It will create a new
+        instance only once.
+        
+        Returns:
+            Nested Singleton object with the list of WebSocketApp objects
+        """
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        """This is an unmodified Singleton constructor. The singleton instance will
+        contain an empty list.
+        """
+        if Singleton.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            self.wsList = []
+            Singleton.__instance = self
+
+    def get_wsList(self):
+        """This method gets the list of WebSocketApp objects.
+
+        Returns:
+            list of WebSocketApp objects
+        """
+        return self.wsList
+
+    def add_ws(self, ws):
+        """This method adds a WebSocketApp object to the list.
+
+        :param ws: WebSocketApp object
 
         Returns:
             None
         """
-    ws.send("hello")
+        self.wsList.append(ws)
+
+    def remove_ws(self, server_address):
+        """This method removes a WebSocketApp object with a given server address from
+        the list.
+
+        :param server_address: server address represented as a string
+
+        Returns:
+            None
+        """
+        for ws in self.wsList:
+            if ws.url == server_address:
+                self.wsList.remove(ws)
+                ws.close()
+                return
+
+    def send_message(self, server_address, message):
+        """This method sends a message to a server with the given server address and
+        message.
+
+        :param server_address: server address represented as a string
+
+        :param message: message represented as a string
+
+        Returns:
+            None
+        """
+        for ws in self.wsList:
+            if ws.url == server_address:
+                ws.send(message)
+                print("Sent " + message + " to " + ws.url)
+                return
+        print(
+            "Message can't be sent to "
+            + server_address
+            + " as there is no established connection."
+        )
 
 
-"""
-These instructions, besides the thread creation, are executed in global scope if you
-want to send messages at a later time. This is because you need the ws object to send
-messages. This object cannot be passed on to other functions using the add_command
-function that the thonny Workbench class offers. A different solution to still use the
-add_command function would be to wrap each method call (Source:
-https://stackoverflow.com/questions/52077459/python-pass-extra-arguments-to-callable).
-Code in global scope is executed before the load_plugin function call.
-"""
-server = askstring("Websocket Server", "")
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp(
-    server, on_message=on_message, on_error=on_error, on_close=on_close,
-)
-ws.on_open = on_open
-t = threading.Thread(target=run_in_background)
-t.daemon = True
-t.start()
+def add_ws_server():
+    """This method gets called if the "Add WS Server" command is clicked in the "tools"
+    menu. It will create a WebSocketApp object with a given server_address and then get
+    the Singleton instance. After that, it will create a new thread and call the
+    "run_in_background" method.
+
+        Returns:
+            None
+        """
+    server_address = askstring("Websocket", "Which server would you like to add?")
+    ws = websocket.WebSocketApp(
+        server_address, on_message=on_message, on_error=on_error, on_close=on_close,
+    )
+    ws.on_open = on_open
+    Singleton.getInstance().add_ws(ws)
+    t = Thread(target=run_in_background)
+    t.daemon = True
+    t.start()
+
+
+def close_ws_server():
+    """This method gets called if the "Close WS Server connection" command is clicked
+    in the "tools" menu. It will get the Singleton instance and call the remove_ws
+    method with the given server address.
+    
+        Returns:
+            None
+        """
+    server_address = askstring("Websocket", "Which server connection should be closed?")
+    Singleton.getInstance().remove_ws(server_address)
+
+
+def send_message():
+    """This method gets called if the "Send Message" command is clicked
+    in the "tools" menu. It will get the Singleton instance and call the send_message
+    method with the given server address and message.
+    
+        Returns:
+            None
+        """
+    server_address = askstring("Websocket", "Which server would you like to message?")
+    message = askstring("Message", "What is your message?")
+    Singleton.getInstance().send_message(server_address, message)
 
 
 def load_plugin():
     """This method gets called if this plugin is in the PYTHONPATH environment variable
-       upon starting thonny.
+       upon starting thonny. It will add the needed commands to the thonny workbench.
 
         Returns:
             None
         """
+    # This method enables DEBUG messages
+    websocket.enableTrace(True)
     get_workbench().add_command(
-        command_id="ws_send",
+        command_id="ws_add",
         menu_name="tools",
-        command_label="Send Hello",
-        handler=send_hello,
+        command_label="Add WS Server",
+        handler=add_ws_server,
+    )
+    get_workbench().add_command(
+        command_id="ws_remove",
+        menu_name="tools",
+        command_label="Close WS Server connection",
+        handler=close_ws_server,
+    )
+    get_workbench().add_command(
+        command_id="ws_remove",
+        menu_name="tools",
+        command_label="Send Message",
+        handler=send_message,
     )
